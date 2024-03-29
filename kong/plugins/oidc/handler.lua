348cef5f1b40602e203fcda0e37bcadfd11cd008
Amir Hodaee <pixozeus@gmail.com>
2024-03-29 03:28:18 +0100

    kong >= 3

--- kong/plugins/oidc/handler.lua
+++ kong/plugins/oidc/handler.lua
@@ -1,18 +1,13 @@
-local BasePlugin = require "kong.plugins.base_plugin"
-local OidcHandler = BasePlugin:extend()
 local utils = require("kong.plugins.oidc.utils")
 local filter = require("kong.plugins.oidc.filter")
 local session = require("kong.plugins.oidc.session")
 
-OidcHandler.PRIORITY = 1000
-
-
-function OidcHandler:new()
-  OidcHandler.super.new(self, "oidc")
-end
+local OidcHandler = {
+	VERSION  = "1.1.0",
+  PRIORITY = 1000,
+}
 
 function OidcHandler:access(config)
-  OidcHandler.super.access(self)
   local oidcConfig = utils.get_options(config, ngx)
 
   if filter.shouldProcessRequest(oidcConfig) then
